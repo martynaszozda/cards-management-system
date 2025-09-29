e7f7d3aae871432a6134c857006ee9840e469382
Martyna Szozda <martyna.szozda5@gmail.com>
2025-09-29 13:17:20 +0200

    Switch to SQLite and add authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CardController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('cards', CardController::class);
 });
